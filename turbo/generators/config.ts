6572da8d1e9ec0897bc5ee78ef2eeea84984cfc4
haswalt <harry.walter@lqdinternet.com>
2026-06-17 16:31:49 +0100

    Initial import

---
+++ turbo/generators/config.ts
@@ -0,0 +1,23 @@
+import type { PlopTypes } from "@turbo/gen";
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setGenerator("component", {
+    description: "Create a new UI component",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Component name (e.g. button):",
+      },
+    ],
+    actions: [
+      {
+        type: "addMany",
+        destination:
+          "{{ turbo.paths.root }}/packages/ui/src/{{kebabCase name}}",
+        base: "templates/component",
+        templateFiles: "templates/component/**",
+      },
+    ],
+  });
+}
