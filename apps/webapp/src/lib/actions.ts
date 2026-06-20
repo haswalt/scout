fdab0c5848a6e65ae31a5a30ff703e5fd0d6055b
haswalt <harry.walter@lqdinternet.com>
2026-06-20 11:20:25 +0100

    fix client usage and caching

--- apps/webapp/src/lib/actions.ts
+++ apps/webapp/src/lib/actions.ts
@@ -1,24 +1,64 @@
-import { getAddressFind } from "@repo/homedata";
+"use server";
+
+import {
+  getAddressFind,
+  getLiveListingsSearch,
+  LiveListingsResponse,
+} from "@repo/homedata";
 
 export async function fetchPostcode(address: string) {
   try {
     const { data } = await getAddressFind({
       cache: "force-cache",
       next: {
-        revalidate: 10,
+        revalidate: 3600,
         tags: ["address"],
       },
+      throwOnError: true,
       query: {
-        query: encodeURIComponent(address),
+        q: address,
       },
     });
 
-    if (data?.suggestions.length) {
-      return data.suggestions[0]?.postcode.toLowerCase().replaceAll(" ", "");
+    const suggestion = data.suggestions.at(0);
+
+    if (suggestion) {
+      return suggestion.postcode.toLowerCase().replaceAll(" ", "");
     }
   } catch (error) {
-    console.error(error);
+    console.error("HomeData address lookup failed", error);
   }
 
   return "po110qw";
+}
+
+export async function fetchProperties(
+  postcode: string,
+): Promise<LiveListingsResponse["results"]> {
+  try {
+    const normalizedPostcode = postcode
+      .replaceAll(" ", "")
+      .toUpperCase()
+      .replace(/(.+)(.{3})$/, "$1 $2");
+
+    const { data } = await getLiveListingsSearch({
+      cache: "force-cache",
+      next: {
+        revalidate: 3600,
+        tags: ["search"],
+      },
+      throwOnError: true,
+      query: {
+        postcode: normalizedPostcode,
+        transaction_type: "Sale",
+        page_size: 4,
+      },
+    });
+
+    return data.results;
+  } catch (error) {
+    console.error("HomeData live listings lookup failed", error);
+  }
+
+  return [];
 }
