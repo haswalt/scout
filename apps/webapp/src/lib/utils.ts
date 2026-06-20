7c171abbe51f7851d880b3d3ca339fcc9491ac55
haswalt <harry.walter@lqdinternet.com>
2026-06-20 21:11:38 +0100

    get color token from decile

--- apps/webapp/src/lib/utils.ts
+++ apps/webapp/src/lib/utils.ts
@@ -4,3 +4,15 @@ export const formatCurrency = (value: number) => {
     currency: "GBP",
   }).format(value);
 };
+
+export const getToneLabel = (decile: number) => {
+  if (decile >= 8) {
+    return "success";
+  }
+
+  if (decile >= 4) {
+    return "warning";
+  }
+
+  return "danger";
+};
