b4f39967995903618fc2eae9a49b2028245e398f
haswalt <harry.walter@lqdinternet.com>
2026-06-20 11:18:17 +0100

    fix API key auth

--- packages/homedata/src/config.ts
+++ packages/homedata/src/config.ts
@@ -1,6 +1,8 @@
 import { type CreateClientConfig } from "./client/client.gen";
 
-export const createClientConfig: CreateClientConfig = (config) => ({
-  ...config,
-  auth: process.env.HOMEDATA_API_KEY,
-});
+export const createClientConfig: CreateClientConfig = (config) => {
+  return {
+    ...config,
+    auth: `Api-Key ${process.env.HOMEDATA_API_KEY}`,
+  };
+};
