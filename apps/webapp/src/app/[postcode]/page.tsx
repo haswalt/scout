37138f70be78d3700009690678b3916e5a9193fc
haswalt <harry.walter@lqdinternet.com>
2026-06-20 21:12:02 +0100

    show results for neighbourhood search

--- apps/webapp/src/app/[postcode]/page.tsx
+++ apps/webapp/src/app/[postcode]/page.tsx
@@ -1,3 +1,33 @@
-export default function ExplorePage() {
-  return <div />;
+import { Header } from "@/components/header";
+import { Box, Container, Flex, styled } from "@repo/ui/jsx";
+import { PropertiesModule } from "@/modules/properties";
+import { DeprivationModule } from "@/modules/deprivation";
+import { LocationModule } from "@/modules/location";
+
+type ExplorePageProps = {
+  params: Promise<{
+    postcode: string;
+  }>;
+};
+
+export default async function ExplorePage({ params }: ExplorePageProps) {
+  const { postcode } = await params;
+
+  return (
+    <Box bg="bg" minH="100dvh">
+      <Header />
+
+      <styled.main>
+        <Container maxW="shell" pt="lg" pb="lg">
+          <Flex direction="column" flex="1" gap="lg">
+            <LocationModule postcode={postcode} />
+
+            <DeprivationModule postcode={postcode} />
+
+            <PropertiesModule postcode={postcode} />
+          </Flex>
+        </Container>
+      </styled.main>
+    </Box>
+  );
 }
